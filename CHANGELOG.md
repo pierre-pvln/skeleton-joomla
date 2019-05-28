aa7964126eeb91c1872a9740a9ecccbbeced0724
Pierre Veelen <pierre@pvln.nl>
2019-05-28 22:22:35 +0200

    all_git_branches script added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 --- 
 #  Changelog Scripts: Generic Joomla! extension structure creation scripts
 ---
+
+<h4>v.0.0.2 2019-05-28</h4>
+<ul>
+<li>Added script for showing all Git branches/li>
+</ul>
+
 <h4>v.0.0.1 2019-04-26</h4>
 <ul>
 <li>Initial version</li>
---
+++ all_git_branches.cmd
@@ -0,0 +1,93 @@
+:: Name:     all_git_branches.cmd
+:: Purpose:  Display the current gitbranch and all branches
+:: Author:   pierre@pvln.nl
+:: Revision: 2019 05 28 - initial version
+::
+:: Required environment variables
+:: ==============================
+:: - VERBOSE         			  how verbose output should be if not set script sets it to YES
+::
+@ECHO off
+SETLOCAL ENABLEEXTENSIONS
+::
+:: BASIC SETTINGS
+:: ==============
+:: Setting the name of the script
+SET me=%~n0
+:: Setting the name of the directory with this script
+SET parent=%~p0
+:: Setting the drive of this commandfile
+SET drive=%~d0
+:: Setting the directory and drive of this commandfile
+SET cmd_dir=%~dp0
+
+::
+:: (re)set environment variables
+::
+
+::
+:: Check if required environment variables are set.
+:: If not set them to a safe default value or exit with error.
+:: 
+IF "%VERBOSE%" == "" (
+   SET VERBOSE=YES
+)
+
+SET FOLDER=code
+CALL:GET_GIT_BRANCH
+
+SET FOLDER=bld
+CALL:GET_GIT_BRANCH
+
+SET FOLDER=stg
+CALL:GET_GIT_BRANCH
+
+SET FOLDER=dpl
+CALL:GET_GIT_BRANCH
+
+SET FOLDER=struc
+CALL:GET_GIT_BRANCH
+
+GOTO CLEAN_EXIT
+
+:ERROR_EXIT
+cd "%cmd_dir%" 
+ECHO *******************
+ECHO %ERROR_MESSAGE%
+ECHO *******************
+   
+:CLEAN_EXIT
+:: Wait some time and exit the script
+::
+PAUSE
+
+::--------------------------------------------------------
+::-- Function section starts below here
+::--
+::-- Inspiration: https://www.dostips.com/DtTutoFunctions.php
+::-- 
+::--------------------------------------------------------
+
+:GET_GIT_BRANCH
+CD ..\%FOLDER%
+
+:: Get current branch in environment variable
+:: https://ss64.com/nt/for_cmd.html
+::
+FOR /F %%G IN ('git rev-parse --abbrev-ref HEAD') DO SET currentGitBranch=%%G
+
+:: remove spaces in name
+:: http://www.dostips.com/DtTipsStringManipulation.php
+:: 
+SET currentGitBranch=%currentGitBranch: =%
+   
+:: show the result 
+IF %VERBOSE%==YES (
+	ECHO [%~n0 ] ... 
+	ECHO [%~n0 ] ... Current local branch for \%FOLDER%: %currentGitBranch%
+	ECHO [%~n0 ] ... 
+	git branch -a
+)
+
+CD "%cmd_dir%"
+goto:eof
