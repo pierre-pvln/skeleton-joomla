a889601561152971700d90a520733b3f46d5f088
Pierre Veelen <pierre@pvln.nl>
2020-03-13 21:07:47 +0100

    changed location of VERBOSE messages

--- create_folder_structure.cmd
+++ create_folder_structure.cmd
@@ -2,7 +2,8 @@
 :: Purpose:  Create the baseline folder structure for development projects
 :: Author:   pierre@pvln.nl
 :: Revision: 2019 04 25 - initial version
-::           2020 03 13 - set subfolder added 
+::           2020 03 13 - set subfolder added
+::			 			- changed location of VERBOSE messages 
 ::
 :: Required environment variables
 :: ==============================
@@ -27,29 +28,43 @@ IF "%extensionFolderName%" == "" (
 
 :: Create folder structure
 ::
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating _bin
-IF NOT EXIST _bin  (md _bin)
+IF NOT EXIST _bin (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating _bin
+	md _bin
+)
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating _set	 
-IF NOT EXIST _set  (md _set)
+IF NOT EXIST _set (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating _set	 
+	md _set
+)
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating _tmp
-IF NOT EXIST _tmp  (md _tmp)
+IF NOT EXIST _tmp (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating _tmp
+	md _tmp
+)
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating bld
-IF NOT EXIST bld   (md bld)
+IF NOT EXIST bld (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating bld
+	md bld
+)
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating dpl
-IF NOT EXIST dpl   (md dpl)
+IF NOT EXIST dpl (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating dpl
+	md dpl
+)
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating misc	 
-IF NOT EXIST misc  (md misc)
+IF NOT EXIST misc (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating misc	 
+	md misc
+)
 	CD misc
 		IF NOT EXIST original  (md original)
 	CD ..
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating code
-IF NOT EXIST code   (md code)
+IF NOT EXIST code (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating code
+	md code
+)
 	CD code
 		IF NOT EXIST src  (md src)
 		IF NOT EXIST doc  (md doc)
@@ -57,15 +72,20 @@ IF NOT EXIST code   (md code)
 		IF NOT EXIST set  (md set)
 	CD ..
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating stg 
-IF NOT EXIST stg   (md stg)
+IF NOT EXIST stg (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating stg 
+	md stg
+)
 
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating struc
-IF NOT EXIST struc (md struc)
+IF NOT EXIST struc (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating struc
+	md struc
+)
 
-
-IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating @_started_
-IF NOT EXIST @_started_*      (md @_started_%date:~9,4%_%date:~6,2%_%date:~3,2%_@)
+IF NOT EXIST @_started_* (
+	IF %VERBOSE%==YES ECHO [%~n0 ] ... Creating @_started_
+	md @_started_%date:~9,4%_%date:~6,2%_%date:~3,2%_@
+)
 
 GOTO CLEAN_EXIT_SUBSCRIPT
 
