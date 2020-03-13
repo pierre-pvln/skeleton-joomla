b063f5e373b42bcb1caf52fd30669e1d10e2dfa5
Pierre Veelen (pvln) <pierre@pvln.nl>
2020-03-13 22:38:22 +0100

    Typo fixed

--- _create-it-all.cmd
+++ _create-it-all.cmd
@@ -72,7 +72,7 @@ IF %VERBOSE%==YES ECHO [%~n0 ] ... Got remote repositories succesfully.
 
 CALL ./struc/create/missing_files.cmd
 IF %ERRORLEVEL% NEQ 0 (
-   SET ERROR_MESSAGE=[ERROR] [%~n0 ] ... Script create_missing_files.cmd returned error %ERRORLEVEL%
+   SET ERROR_MESSAGE=[ERROR] [%~n0 ] ... Script missing_files.cmd returned error %ERRORLEVEL%
    GOTO ERROR_EXIT
 )
 IF %VERBOSE%==YES ECHO [%~n0 ] ... Missing files created succesfully.
