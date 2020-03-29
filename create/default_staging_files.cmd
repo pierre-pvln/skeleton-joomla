f1eee57809ef10bc661252dee554f2d9f9596716
Pierre Veelen <pierre@pvln.nl>
2020-03-29 16:56:04 +0200

    text updated

--- create/default_staging_files.cmd
+++ create/default_staging_files.cmd
@@ -7,8 +7,6 @@
 :: ==============================
 :: - VERBOSE                      how verbose output should be if not set script sets it to YES
 :: - extensionFolderName          the name of the extension based on the top level foldername
-:: - extensionType				  the type of extension
-:: - extensionName				  the name of the extension
 
 @ECHO off
 SETLOCAL ENABLEEXTENSIONS
@@ -23,12 +21,6 @@ IF "%VERBOSE%" == "" (
 IF "%extensionFolderName%" == "" (
    SET ERROR_MESSAGE=[ERROR] [%~n0 ] ... extensionFolderName environment variable not set.
    GOTO ERROR_EXIT_SUBSCRIPT
-)
-IF "%extensionName%" == "" (
-   SET extensionName=^<extensionname^>
-)
-IF "%extensionType%" == "" (
-   SET extensionType=^<extensiontype^>
 )
 
 :: Create default files
@@ -47,47 +39,45 @@ IF NOT EXIST "README.md" (
 	ECHO --- 
 	ECHO # Generic Staging Process Scripts
 	ECHO --- 		
-	ECHO Generic deployment scripts for Joomla! website extensions.^<br/^>
+	ECHO Generic staging scripts for Joomla! website extensions.^<br/^>
 	ECHO ^<br/^>
 	ECHO * Documentation and download extension: http://www.pvln.nl/stage-joomla-extensions^<br/^>
 	ECHO ^<br/^>
-	ECHO Below folder structure should be present on the workstation on which development is done:
+	ECHO Below folder structure should be present on the workstation on which development is done:^<br/^>
 	ECHO.
+	ECHO __Managed using git__
 	ECHO ``` 
-	ECHO %extensionFolderName%\code           folder with all code related items __git managed__
-	ECHO %extensionFolderName%\code\src       folder with the code for the Joomla! extension, which gets installed on the Joomla! website
-	ECHO %extensionFolderName%\code\doc       documentation related to the source code
-	ECHO %extensionFolderName%\code\set       specific settings for the extension
-	ECHO %extensionFolderName%\code\tst       tests for the source code
+	ECHO ^<extensionname^>\code           folder with all code related items
+	ECHO ^<extensionname^>\code\src       folder with the code for the Joomla! extension, which gets installed on the Joomla! website
+	ECHO ^<extensionname^>\code\doc       documentation related to the source code
+	ECHO ^<extensionname^>\code\set       specific settings for the extension
+	ECHO ^<extensionname^>\code\tst       tests for the source code
 	ECHO.			 
-	ECHO %extensionFolderName%\bld            folder with scripts to build the extension zipfile __git managed__
+	ECHO ^<extensionname^>\bld            folder with scripts to build the extension zipfile
 	ECHO.		 
-	ECHO %extensionFolderName%\stg            folder with scripts to stage it to the update and download webserver ^<- THIS CODE __git managed__
+	ECHO ^<extensionname^>\stg            folder with scripts to stage it to the update and download webserver ^<- THIS CODE
 	ECHO.
-	ECHO %extensionFolderName%\dpl            folder with generic deploy scripts for Joomla! website extensions __git managed__
+	ECHO ^<extensionname^>\dpl            folder with generic deploy scripts for Joomla! website extensions
 	ECHO. 
-	ECHO %extensionFolderName%\struc          scripts to create the Joomla! deployment skeleton __git managed__
+	ECHO ^<extensionname^>\struc          scripts to create the Joomla! deployment skeleton
+	ECHO ```
 	ECHO.
-	ECHO %extensionFolderName%\misc           folder with relevant information links and inspiration, but not relevant for code
-	ECHO %extensionFolderName%\misc\original  if relevant the original code which is changed in \code\src
+	ECHO ^<br/^>
 	ECHO.
-	ECHO %extensionFolderName%\_set           folder with settings used by various scripts. Files in this folder are shared between different scripts.
+    ECHO __Not managed, only present at development workstation__
+    ECHO ```
+	ECHO ^<extensionname^>\misc           folder with relevant information links and inspiration, but not relevant for code
+	ECHO ^<extensionname^>\misc\original  if relevant the original code which is changed in \code\src
 	ECHO.
-	ECHO %extensionFolderName%\_bin           folder with the output files from scripts. Files in this folder are shared between different scripts.
+	ECHO ^<extensionname^>\_set           folder with settings used by various scripts. Files in this folder are shared between different scripts.
 	ECHO.
-	ECHO %extensionFolderName%\_tmp           Folder used to place temporary output files from scripts. Files in this folder are shared between different scripts.
+	ECHO ^<extensionname^>\_bin           folder with the output files from scripts. Files in this folder are shared between different scripts.
+	ECHO.
+	ECHO ^<extensionname^>\_tmp           Folder used to place temporary output files from scripts. Files in this folder are shared between different scripts.
 	ECHO ``` 
  	ECHO ^<br/^>
-	ECHO The downloadable extension .zip file is available at:
-	ECHO ```
-	ECHO download.pvln.nl/joomla/%extensionType%/%extensionName%/
-	ECHO ```
-	ECHO ^<br/^>
-	ECHO The downloadable extension update .xml file is available at:
-	ECHO ```
-	ECHO update.pvln.nl/joomla/%extensionType%/%extensionName%/
-	ECHO ```
 	) >README.md
+
 IF NOT EXIST "CHANGELOG.md" (
 	ECHO --- 
 	ECHO #  Changelog Scripts: Generic Joomla! extension staging scripts
@@ -97,6 +87,7 @@ IF NOT EXIST "CHANGELOG.md" (
 	ECHO ^<li^>Initial version^</li^>
 	ECHO ^</ul^>
 	) >CHANGELOG.md
+
 IF %VERBOSE%==YES ECHO [%~n0 ] ... Files for %extensionFolderPath%/stg created succesfully.
 
 GOTO CLEAN_EXIT_SUBSCRIPT
